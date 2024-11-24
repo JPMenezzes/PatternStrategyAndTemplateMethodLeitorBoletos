a150e526027c53820aca3d9df5755602de7b8a26
menezes <jpgam321@gmail.com>
2024-11-24 17:05:22 -0300

    LeituraRetornoBancoBradesco implementado em Programação Funcional
    Signed-off-by: menezes <jpgam321@gmail.com>

--- LeitorBoleto/src/main/java/Principal.java
+++ LeitorBoleto/src/main/java/Principal.java
@@ -1,3 +1,4 @@
+import br.com.ifto.LeituraRetornoBancoBradesco;
 import br.com.ifto.LeituraRetornoBancoBrasil;
 import br.com.ifto.ProcessadorBoletos;
 
@@ -6,10 +7,16 @@ import java.net.URISyntaxException;
 
 public class Principal {
     public static void main(String[] args) throws URISyntaxException {
-        final var processador = new ProcessadorBoletos(LeituraRetornoBancoBrasil::processarLinhaArquivo);
+        final var processadorBrasil = new ProcessadorBoletos(LeituraRetornoBancoBrasil::processarLinhaArquivo);
         URI caminhoArquivo = Principal.class.getResource("banco-brasil-1.csv").toURI();
-        System.out.println("Lendo arquivo " + caminhoArquivo + "\n");
+        System.out.println("\n" + "Lendo arquivo " + caminhoArquivo + "\n");
 
-        processador.processar(caminhoArquivo);
+        processadorBrasil.processar(caminhoArquivo);
+
+        final var processadorBradesco = new ProcessadorBoletos(LeituraRetornoBancoBradesco::processarLinhaArquivo);
+        caminhoArquivo = Principal.class.getResource("bradesco-1.csv").toURI();
+        System.out.println("\n" + "Lendo arquivo " + caminhoArquivo + "\n");
+
+        processadorBradesco.processar(caminhoArquivo);
     }
 }
